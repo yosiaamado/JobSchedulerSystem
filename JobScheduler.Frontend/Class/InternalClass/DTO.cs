05e7ed0ec33d322a83f7cace08425ba1d35222a3
Yosia Amado Suwitomo <YosiaAS@maybank.co.id>
2025-05-28 17:07:22 +0700

    add bootstrap and few pages

---
+++ JobScheduler.Frontend/Class/InternalClass/DTO.cs
@@ -0,0 +1,13 @@
+﻿namespace JobScheduler.Frontend.Class.InternalClass
+{
+    public class ConnectionDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Host { get; set; } = string.Empty;
+        public string PortString { get; set; } = "";
+        public int Port { get; set; }
+        public string DatabaseName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ JobScheduler.Frontend/Class/InternalClass/LayoutClass.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace JobScheduler.Frontend.Class.InternalClass
+{
+    public class NavItem
+    {
+        public string Title { get; set; } = "";
+        public string Icon { get; set; } = "";
+        public string Href { get; set; } = "";
+        public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+    }
+}
